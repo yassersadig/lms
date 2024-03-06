197fb2503bfe42da829706eeb26bbe6416394cd6
Yasser Ali <yasseralbalola@gmail.com>
2024-03-06 23:08:07 +0400

    home screen frontend

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,59 +1,151 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-    Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
 } from "@material-tailwind/react";
+import Star from "./components/Start";
 
 const courses = [
-    { 'name': 'introduction to react', 'description': 'a course on react', 'thumbnail': 'https://picsum.photos/150?random=1' },
-    { 'name': 'introduction to python', 'description': 'a course on python', 'thumbnail': 'https://picsum.photos/150?random=2' },
-    { 'name': 'introduction to javascript', 'description': 'a course on javascript', 'thumbnail': 'https://picsum.photos/150?random=3' },
-    { 'name': 'introduction to java', 'description': 'a course on java', 'thumbnail': 'https://picsum.photos/150?random=4' },
-    { 'name': 'introduction to c++', 'description': 'a course on c++', 'thumbnail': 'https://picsum.photos/150?random=5' },
-    { 'name': 'introduction to c', 'description': 'a course on c', 'thumbnail': 'https://picsum.photos/150?random=6' },
-    { 'name': 'introduction to c#', 'description': 'a course on c#', 'thumbnail': 'https://picsum.photos/150?random=7' },
-    { 'name': 'introduction to ruby', 'description': 'a course on ruby', 'thumbnail': 'https://picsum.photos/150?random=8' },
-    { 'name': 'introduction to go', 'description': 'a course on go', 'thumbnail': 'https://picsum.photos/150?random=9' },
-    { 'name': 'introduction to rust', 'description': 'a course on rust', 'thumbnail': 'https://picsum.photos/150?random=10' },
-    { 'name': 'introduction to typescript', 'description': 'a course on typescript', 'thumbnail': 'https://picsum.photos/150?random=11' },
-    { 'name': 'introduction to kotlin', 'description': 'a course on kotlin', 'thumbnail': 'https://picsum.photos/150?random=12' },
-    { 'name': 'introduction to swift', 'description': 'a course on swift', 'thumbnail': 'https://picsum.photos/150?random=13' },
-    { 'name': 'introduction to dart', 'description': 'a course on dart', 'thumbnail': 'https://picsum.photos/150?random=14' },
-    { 'name': 'introduction to flutter', 'description': 'a course on flutter', 'thumbnail': 'https://picsum.photos/150?random=15' },
-    { 'name': 'introduction to react native', 'description': 'a course on react native', 'thumbnail': 'https://picsum.photos/150?random=16' },
+  {
+    name: "introduction to react",
+    description: "a course on react",
+    thumbnail: "https://picsum.photos/150?random=1",
+    author: "John Doe",
+    rating: 4.5,
+  },
+  {
+    name: "introduction to python",
+    description: "a course on python",
+    thumbnail: "https://picsum.photos/150?random=2",
+    author: "Jane Doe",
+    rating: 4.5,
+  },
+  {
+    name: "introduction to javascript",
+    description: "a course on javascript",
+    thumbnail: "https://picsum.photos/150?random=3",
+    author: "John Doe",
+  },
+  {
+    name: "introduction to java",
+    description: "a course on java",
+    thumbnail: "https://picsum.photos/150?random=4",
+  },
+  {
+    name: "introduction to c++",
+    description: "a course on c++",
+    thumbnail: "https://picsum.photos/150?random=5",
+  },
+  {
+    name: "introduction to c",
+    description: "a course on c",
+    thumbnail: "https://picsum.photos/150?random=6",
+  },
+  {
+    name: "introduction to c#",
+    description: "a course on c#",
+    thumbnail: "https://picsum.photos/150?random=7",
+  },
+  {
+    name: "introduction to ruby",
+    description: "a course on ruby",
+    thumbnail: "https://picsum.photos/150?random=8",
+  },
+  {
+    name: "introduction to go",
+    description: "a course on go",
+    thumbnail: "https://picsum.photos/150?random=9",
+  },
+  {
+    name: "introduction to rust",
+    description: "a course on rust",
+    thumbnail: "https://picsum.photos/150?random=10",
+  },
+  {
+    name: "introduction to typescript",
+    description: "a course on typescript",
+    thumbnail: "https://picsum.photos/150?random=11",
+  },
+  {
+    name: "introduction to kotlin",
+    description: "a course on kotlin",
+    thumbnail: "https://picsum.photos/150?random=12",
+  },
+  {
+    name: "introduction to swift",
+    description: "a course on swift",
+    thumbnail: "https://picsum.photos/150?random=13",
+  },
+  {
+    name: "introduction to dart",
+    description: "a course on dart",
+    thumbnail: "https://picsum.photos/150?random=14",
+  },
+  {
+    name: "introduction to flutter",
+    description: "a course on flutter",
+    thumbnail: "https://picsum.photos/150?random=15",
+  },
+  {
+    name: "introduction to react native",
+    description: "a course on react native",
+    thumbnail: "https://picsum.photos/150?random=16",
+  },
 ];
 
-
 const Home = () => {
-    return (
-        <div className="flex flex-col h-full justify-center">
-            {courses.map((course, index) => <div key={index} className="flex items-center justify-center border-b">
-                <Card className="w-3/4 h-32 flex-row mt-3 mb-3 rounded-none shadow-none">
-                    <CardHeader
-                        shadow={false}
-                        floated={false}
-                        className="m-0 shrink-0 rounded-none"
-                    >
-                        <img
-                            src={course.thumbnail}
-                            alt="card-image"
-                            className="h-full w-full object-cover rounded-none"
-                            width={150}
-                            height={150}
-                        />
-                    </CardHeader>
-                    <CardBody>
-                        <Typography variant="span" color="gray" className="mb-4 uppercase w-full">
-                            {course.name}
-                        </Typography>
-                    </CardBody>
-                </Card>
-            </div>)}
+    const navigate = useNavigate();
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+  return (
+    <div className="flex flex-col h-full">
+      {courses.map((course, index) => (
+        <div key={index} className="flex mx-4 border-b" onClick={() => handleNavigation('/courses')}>
+          <Card className="w-full h-32 flex-row mt-3 mb-3 rounded-none shadow-none">
+            <CardHeader
+              shadow={false}
+              floated={false}
+              className="m-0 shrink-0 rounded-none w-32 h-32"
+            >
+              <img
+                src={course.thumbnail}
+                alt="card-image"
+                className="h-full w-full object-cover rounded-none"
+                width={150}
+                height={150}
+              />
+            </CardHeader>
+            <CardBody className="w-full px-2 py-0 flex flex-col h-full justify-between">
+              <Typography className="w-full h-1/4 overflow-hidden whitespace-nowrap overflow-ellipsis text-lg font-bold text-black">
+                {course.name}
+              </Typography>
+              <Typography color="gray" className="w-full h-1/4">
+                {course.description}
+              </Typography>
+              <Typography color="gray" className="w-full h-1/4">
+                By: {course.author}
+              </Typography>
+              <Typography className="w-full h-1/4">
+                <span className="flex items-center">
+                  <span className="mr-2">{course.rating || "0.0"}</span>
+                  <Star fill={course.rating >= 1} />
+                  <Star fill={course.rating >= 2} />
+                  <Star fill={course.rating >= 3} />
+                  <Star fill={course.rating >= 4} />
+                  <Star fill={course.rating >= 5} />
+                </span>
+              </Typography>
+            </CardBody>
+          </Card>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Home;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,126 +1,152 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios, { AxiosError } from 'axios';
-import { useFormik } from 'formik';
-import useSignIn from 'react-auth-kit/hooks/useSignIn';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios, { AxiosError } from "axios";
+import { useFormik } from "formik";
+import useSignIn from "react-auth-kit/hooks/useSignIn";
 import BounceLoader from "react-spinners/BounceLoader";
 import { Link } from "react-router-dom";
 
-
-
 const Login = () => {
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    const signIn = useSignIn();
-    const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const signIn = useSignIn();
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (values) => {
-        try {
-            setLoading(true);
-            const response = await axios.post(process.env.REACT_APP_SERVER_URL + '/login', values);
-            if (signIn({
-                auth: {
-                    token: response.data.token,
-                    tokenType: 'Bearer',
-                },
-                userState: {
-                    name: response.data.name,
-                    email: response.data.email
-                }
-            })) {
-                navigate('/courses');
-                setLoading(false);
-            }
+  const handleSubmit = async (values) => {
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        process.env.REACT_APP_SERVER_URL + "/login",
+        values
+      );
+      if (
+        signIn({
+          auth: {
+            token: response.data.token,
+            tokenType: "Bearer",
+          },
+          userState: {
+            name: response.data.name,
+            email: response.data.email,
+          },
+        })
+      ) {
+        setLoading(false);
+        navigate("/courses");
+      }
+    } catch (err) {
+      if (err && err instanceof AxiosError) {
+        setError(err.response?.data.message);
+      } else if (err instanceof Error) {
+        setError(err.message);
+      }
+      setLoading(false);
+    }
+  };
 
-        } catch (err) {
-            if (err && err instanceof AxiosError) {
-                setError(err.response?.data.message);
-            } else if (err instanceof Error) {
-                setError(err.message);
-            }
-            setLoading(false);
-        }
-    };
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+    return errors;
+  };
 
-    const validate = values => {
-        const errors = {};
-        if (!values.email) {
-            errors.email = 'Email is required';
-        } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-            errors.email = 'Invalid email address';
-        }
-        if (!values.password) {
-            errors.password = 'Password is required';
-        }
-        return errors;
-    };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+    validate,
+  });
 
-
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-        onSubmit: values => {
-            handleSubmit(values);
-        },
-        validate,
-    });
-
-    return (
-        <div className="flex flex-col h-full items-center justify-center">
-            {loading ? <div className="absolute z-10 before:block before:fixed before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:bg-opacity-35">
-                <BounceLoader color={"#3b82f6"} loading={loading} size={100} />
-            </div> : null}
-            <div className="w-full max-w-xs shadow-xl rounded-lg px-8 pt-6 pb-8 mb-4">
-                <div className="text-center text-2xl mb-4">Login</div>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
-                            id="email"
-                            name="email"
-                            type="email"
-                            placeholder="yasser@mail.com"
-                            value={formik.values.email}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                        {formik.touched.email && formik.errors.email ? <div className="text-center text-red-300">{formik.errors.email}</div> : null}
-
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
-                            id="password"
-                            name="password"
-                            type="password"
-                            placeholder="******"
-                            value={formik.values.password}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                        {formik.touched.password && formik.errors.password ? <div className="text-center text-red-300">{formik.errors.password}</div> : null}
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                            Sign In
-                        </button>
-                    </div>
-                    {error ? <div className="text-center text-red-300">{error}</div> : null}
-                </form>
-                <div className="text-center mt-4">Don't have an account? <Link to="/signup" className="text-blue-500">Sign Up</Link></div>
-            </div>
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+      {loading ? (
+        <div className="absolute z-10 before:block before:fixed before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:bg-opacity-35">
+          <BounceLoader color={"#3b82f6"} loading={loading} size={100} />
         </div>
-    );
+      ) : null}
+      <div className="w-full max-w-xs shadow-xl rounded-lg px-8 pt-6 pb-8 mb-4">
+        <div className="text-center text-2xl mb-4">Login</div>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="yasser@mail.com"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-center text-red-300">
+                {formik.errors.email}
+              </div>
+            ) : null}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="******"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-center text-red-300">
+                {formik.errors.password}
+              </div>
+            ) : null}
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </div>
+          {error ? (
+            <div className="text-center text-red-300">{error}</div>
+          ) : null}
+        </form>
+        <div className="text-center mt-4">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
